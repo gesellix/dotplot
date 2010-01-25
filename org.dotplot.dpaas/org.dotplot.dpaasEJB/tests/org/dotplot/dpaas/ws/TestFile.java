ec558976765f1948912bdfd49811e70a0a9de19d
Christian Gerhardt <case42@gmx.net>
2010-01-25 21:50:14 +0000

    stated tests for dpaasEJB

---
+++ org.dotplot.dpaas/org.dotplot.dpaasEJB/tests/org/dotplot/dpaas/ws/TestFile.java
@@ -0,0 +1,22 @@
+package org.dotplot.dpaas.ws;
+
+import junit.framework.TestCase;
+
+public class TestFile extends TestCase {
+
+	protected void setUp() throws Exception {
+	}
+
+	public void testGetSetFilename() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetSetContent() {
+		fail("Not yet implemented");
+	}
+
+
+	public void testGetSetFileType() {
+		fail("Not yet implemented");
+	}
+}
